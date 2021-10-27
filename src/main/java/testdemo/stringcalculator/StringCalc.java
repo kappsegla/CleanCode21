b1020081e1a58438113f543e4a474ddd8f9c2fa8
Martin Blomberg <martin.blomberg@outlook.com>
2021-10-27 09:45:20 +0200

    Adds add method to StringCalc

--- src/main/java/testdemo/stringcalculator/StringCalc.java
+++ src/main/java/testdemo/stringcalculator/StringCalc.java
@@ -2,4 +2,7 @@ package testdemo.stringcalculator;
 
 public class StringCalc {
 
+    public int add(String s) {
+        return 0;
+    }
 }
--- src/test/java/testdemo/stringcalculator/StringCalcTest.java
+++ src/test/java/testdemo/stringcalculator/StringCalcTest.java
@@ -3,11 +3,20 @@ package testdemo.stringcalculator;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //https://kata-log.rocks/string-calculator-kata
 
 public class StringCalcTest {
     StringCalc stringCalc = new StringCalc();
+
+    @Test
+    void givenAnEmptyStringShouldReturnZero() {
+        assertEquals(0, stringCalc.add(""));
+    }
+
+
+
 
 }
