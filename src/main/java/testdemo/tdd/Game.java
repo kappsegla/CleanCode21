215bfe72dd56591346e9f14f3188b5c23627bf40
Martin Blomberg <martin.blomberg@outlook.com>
2021-12-13 15:35:21 +0100

    Adds strike tests

--- src/main/java/testdemo/tdd/Game.java
+++ src/main/java/testdemo/tdd/Game.java
@@ -1,9 +1,20 @@
 package testdemo.tdd;
 
 public class Game {
-        int totalScore = 0;
-    public void roll(int i) {
-        totalScore += i;
+    public static final int ALL_PINS = 10;
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pinsKnockedDown) {
+        if (isFirstRollInFrame() && pinsKnockedDown == ALL_PINS) {
+            rolls[currentRoll++] = pinsKnockedDown;
+            currentRoll++;
+        } else
+            rolls[currentRoll++] = pinsKnockedDown;
+    }
+
+    private boolean isFirstRollInFrame() {
+        return currentRoll % 2 == 0;
     }
 
     public int score() {
--- src/test/java/testdemo/tdd/GameTest.java
+++ src/test/java/testdemo/tdd/GameTest.java
@@ -6,26 +6,59 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
+    private final Game game;
+
+    public GameTest() {
+        game = new Game();
+    }
 
     @Test
     void rolling20TimesWithNoPinsKnockedDownShouldReturnScoreZero() {
-        Game game = new Game();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        multipleRolls(20, 0);
 
         assertEquals(0, game.score());
     }
 
     @Test
     void rolling20TimesWithOnePinKnockedDownShouldReturnScore20() {
-        Game game = new Game();
-
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        multipleRolls(20, 1);
 
         assertEquals(20, game.score());
+    }
+
+    @Test
+    void spareTest() {
+        game.roll(5);
+        game.roll(5);
+        game.roll(2);
+        multipleRolls(17, 0);
+
+        assertEquals(14, game.score());
+    }
+
+    @Test
+    void strikeTest() {
+        game.roll(10);
+        game.roll(5);
+        game.roll(2);
+        multipleRolls(16, 0);
+
+        assertEquals(24, game.score());
+    }
+
+    @Test
+    void spareInLastRoundTest() {
+        multipleRolls(18, 0);
+        game.roll(5);
+        game.roll(5);
+        game.roll(2);
+
+        assertEquals(12, game.score());
+    }
+
+    private void multipleRolls(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            game.roll(pins);
+        }
     }
 }
