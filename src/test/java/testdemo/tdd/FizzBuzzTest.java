0780fb7701039d6da8260a1c55d2898821dc477c
Martin Blomberg <martin.blomberg@outlook.com>
2021-12-13 13:13:30 +0100

    Adds many tests for FizzBuzz

--- src/test/java/testdemo/tdd/FizzBuzzTest.java
+++ src/test/java/testdemo/tdd/FizzBuzzTest.java
@@ -2,6 +2,8 @@ package testdemo.tdd;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
